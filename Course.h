2a6704414b676576a7048409c48b35ffea71e753
abdullah-ibrahim16003238 <abdullahmoatazbellah@gmail.com>
2025-11-11 09:44:46 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -1,22 +1,32 @@
 #ifndef COURSE_H
 #define COURSE_H
-
 #include <string>
 #include "Student.h"
 using namespace std;
-//#write your code here
 
+class Person {
+private:
+    string name;
+    int id;
 
+public:
+    Person() {
+        name = "";
+        id = 0;
+    }
+Person(string name, int id) {
+        this->name = name;
+        this->id = id;
+    }
 
-
-
-
-
-
-
-
-
-
-
+~Person() {}
+void setName(string name) { this->name = name; }
+void setId(int id) { this->id = id; }
+string getName() const { return name; }
+int getId() const { return id; }
+virtual void display() const {
+        cout << "Name: " << name << " (ID: " << id << ")" << endl;
+    }
+};
 
 #endif
