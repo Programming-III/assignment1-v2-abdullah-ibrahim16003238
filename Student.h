671c7666c0ec3e7fe1261f849ec89e48aa2f42dc
abdullah-ibrahim16003238 <abdullahmoatazbellah@gmail.com>
2025-11-11 09:48:55 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -4,18 +4,35 @@
 #include "Person.h"
 #include <string>
 #using namespace std;
-//#write student class here
+class Student : public Person {
+private:
+    int yearLevel;
+    string major;
 
+public:
 
+    Student() : Person() {
+        yearLevel = 0;
+        major = "";
+    }
+Student(string name, int id, int yearLevel, string major)
+        : Person(name, id) {
+        this->yearLevel = yearLevel;
+        this->major = major;
+    }
+~Student() {}
+    void setYearLevel(int level) { yearLevel = level; }
+    void setMajor(string m) { major = m; }
 
+    int getYearLevel() const { return yearLevel; }
+    string getMajor() const { return major; }
 
-
-
-
-
-
-
-
-
+    void display() const override {
+        cout << "Student Info:" << endl;
+        Person::display();
+        cout << "Year: " << yearLevel << endl;
+        cout << "Major: " << major << endl;
+    }
+};
 
 #endif
